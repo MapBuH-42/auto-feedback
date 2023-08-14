222e7298f1f242b8c723f6771a7eaa3348916917
Hristo Nikolov <139870489+MapBuH-42@users.noreply.github.com>
2023-08-14 14:49:13 +0000

    saving changes

--- CORE/main.py
+++ CORE/main.py
@@ -2,7 +2,7 @@
 from sender import send_email
 
 if __name__ == "__main__":
-    sender_email = "auto.test.feedback.bot" #REPLACE WITH SENDER EMAIL ADDRESS
+    sender_email = "auto.test.feedback.bot@gmail.com" #REPLACE WITH SENDER EMAIL ADDRESS
     sender_password = "zYSd.6hLQUrtS$A" #REPLACE WITH SENDER EMAIL PASSWORD - EXPORT REF FOR SECURITY
 
     recipients = [
